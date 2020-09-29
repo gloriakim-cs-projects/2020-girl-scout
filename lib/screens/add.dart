44044c8156a7f9d22a1a1cea4ee75c0f9504d0b5
Gloria Kim <gloriakim.cs.projects@gmail.com>
2020-09-28 23:56:38 -0500

    Mostly completed add.dart; need to do checklist and save button

--- lib/screens/add.dart
+++ lib/screens/add.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:girl_scout_simple/components/constants.dart';
 
 class Add extends StatefulWidget {
-
+  //TODO: complete parameters
   Add({@required this.title});
   String title; //(ex) Add Member
 
@@ -16,6 +16,9 @@ class _AddState extends State<Add> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: kWhiteColor, //change your color here
+        ),
         title: Text(
           "Add Member",
           style: TextStyle(
@@ -24,13 +27,124 @@ class _AddState extends State<Add> {
             fontSize: 30.0,
           ),
         ),
-        backgroundColor: kDartGreyColor,),
+        backgroundColor: kDarkGreyColor,),
       body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Text("57 Collections", style: Theme.of(context).textTheme.bodyText1,),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text("Title", style: Theme.of(context).textTheme.headline2,),
+              TextField(
+                decoration: InputDecoration(
+                    hintText: 'Enter a search term'
+                ),
+              ),
+              SizedBox(height: 10),
+              Text("Category", style: Theme.of(context).textTheme.headline2,),
+              TextField(
+                decoration: InputDecoration(
+                    hintText: 'Enter a search term'
+                ),
+              ),
+              SizedBox(height: 10),
+              Text("Sub-Category", style: Theme.of(context).textTheme.headline2,),
+              TextField(
+                decoration: InputDecoration(
+                    hintText: 'Enter a search term'
+                ),
+              ),
+              SizedBox(height: 10),
+              Text("Grade", style: Theme.of(context).textTheme.headline2,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  GradeButton(grade: "Daisy"),
+                  GradeButton(grade: "Brownie"),
+                  GradeButton(grade: "Junior"),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  GradeButton(grade: "Cadette"),
+                  GradeButton(grade: "Senior"),
+                  GradeButton(grade: "Ambassador"),
+                ],
+              ),
+              SizedBox(height: 10),
+              Text("Photo", style: Theme.of(context).textTheme.headline2,),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  PhotoButton(icon: 58055, name: "Gallery"),
+                  SizedBox(width: 10),
+                  PhotoButton(icon: 58386, name: "Camera"),
+                ],
+              ),
+              SizedBox(height: 10),
+              Text("Checklist", style: Theme.of(context).textTheme.headline2,),
+              //TODO: add checklist
+              //TODO: add save button
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class GradeButton extends StatefulWidget {
+
+  GradeButton({@required this.grade});
+  final String grade;
+
+  @override
+  _GradeButtonState createState() => _GradeButtonState();
+}
+
+class _GradeButtonState extends State<GradeButton> {
+  bool pressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      MaterialButton(
+      child: Text(widget.grade),
+      //TODO: using minWidth will overflow for some phones... use proportional size of screen.
+      minWidth: 120,
+      textColor: pressed? kDarkGreyColor : kWhiteColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: new BorderRadius.circular(8.0),
+          side: BorderSide(color: pressed? kDarkGreyColor : kDarkGreyColor, width: 2.0),
+      ),
+      color: pressed ? kWhiteColor : kDarkGreyColor,
+      onPressed: () => setState(() => pressed = !pressed),
+    );
+  }
+}
+
+class PhotoButton extends StatelessWidget {
+
+  PhotoButton({@required this.icon, @required this.name});
+  final int icon; //Icons.folder
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      //TODO: include onPressed functionality
+      onPressed: () => {},
+      color: Colors.black12,
+      shape: RoundedRectangleBorder(
+        borderRadius: new BorderRadius.circular(8.0),
+      ),
+      padding: EdgeInsets.all(20.0),
+      child: Column( // Replace with a Row for horizontal icon + text
+        children: <Widget>[
+          Icon(IconData(icon, fontFamily: 'MaterialIcons'), size: 60.0,),
+          Text(name, style: TextStyle(color: Colors.black),),
+        ],
       ),
     );
   }
