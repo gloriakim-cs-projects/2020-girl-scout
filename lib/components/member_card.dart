50b7dbb1ab18584f2ee8584332b1a757d401bf5d
Christiana <christianaramey@gmail.com>
2020-12-02 14:47:19 -0600

    Created on tap function for menbers

--- lib/components/member_card.dart
+++ lib/components/member_card.dart
@@ -3,32 +3,38 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:girl_scout_simple/components/constants.dart';
 import 'package:girl_scout_simple/models.dart';
+import 'package:girl_scout_simple/components/globals.dart';
+import 'package:girl_scout_simple/screens/member_info.dart';
 
 //import 'globals.dart';
 
 class AnimatedMemberCard extends StatefulWidget {
-  final String parentPage;
-  final String name;
-  final String team;
-  final gradeEnum grade;
-  final String birthMonth;
-  final int birthDay;
-  final int birthYear;
-  final int numBadgesCompleted;
-  final String imageLocation;
 
-  const AnimatedMemberCard({Key key, this.parentPage, this.name, this.team, this.grade, this.numBadgesCompleted, this.imageLocation, this.birthMonth, this.birthDay, this.birthYear}): super(key: key);
+  final Data data;
 
+  AnimatedMemberCard({@required this.data});
   @override
   _AnimatedMemberCard createState() => _AnimatedMemberCard();
 }
 
 class _AnimatedMemberCard extends State<AnimatedMemberCard> {
+
+  String name;
+  String team;
+  String imageLocation;
+
   @override
   Widget build(BuildContext context) {
+    name = widget.data.name;
+    team = widget.data.team;
+    imageLocation = widget.data.photoLocation;
+
     return AnimatedContainer(
       margin: EdgeInsets.fromLTRB(15, 15, 15, 0),
-      width: MediaQuery.of(context).size.width * 0.91,
+      width: MediaQuery
+          .of(context)
+          .size
+          .width * 0.91,
       decoration: BoxDecoration(
         color: kWhiteColor,
         border: Border.all(
@@ -49,47 +55,60 @@ class _AnimatedMemberCard extends State<AnimatedMemberCard> {
       child: Padding(
         padding: const EdgeInsets.all(15.0),
         child: InkWell(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              //parentPage == 'Setting' ? ExcludeTitle() : IncludeTitle(title: title, subtitle: subtitle),
-              //show only if subtitle is not null ('')
-              Expanded(
-                flex: 4,
-                  child: Image.file(File(widget.imageLocation)),
-              ),
-              SizedBox(width: 10.0),
-              Expanded(
-                flex: 6,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                //parentPage == 'Setting' ? ExcludeTitle() : IncludeTitle(title: title, subtitle: subtitle),
+                //show only if subtitle is not null ('')
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: FileImage(File(imageLocation)),
+                          fit: BoxFit.scaleDown
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10.0),
+                Expanded(
+                  flex: 7,
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Text(widget.name, style: Theme
-                        .of(context)
-                        .textTheme
-                        .headline2,),
-                    Text(widget.team, style: Theme
-                        .of(context)
-                        .textTheme
-                        .subtitle1,),
-                  ]
-              ),
-              ),
-              SizedBox(height: 20.0),
-              Column(
-                  children: <Widget>[
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text(name, style: Theme
+                            .of(context)
+                            .textTheme
+                            .headline2,),
+                        SizedBox(height: 20.0),
+                        Text(team, style: Theme
+                            .of(context)
+                            .textTheme
+                            .subtitle1,),
+                      ]
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Column(
+                    children: <Widget>[
 //TODO add number of bagdes completed in an elegant way, can be a button that appears when scout is ready to be moved to higher rank
-                  ]
-              ),
-            ],
-          ),
-          onTap: (){
-            setState((){
-              //update();
-            });
-            //TODO create funtion so that if state is triggered, bring up edit page with populated information for tapped scout
-          }
+                    ]
+                ),
+              ],
+            ),
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(
+                  builder: (context) => new MemberInfo(data: widget.data)));
+              setState(() {
+                //update();
+              });
+              //TODO create funtion so that if state is triggered, bring up edit page with populated information for tapped scout
+            }
         ),
       ),
     );
---
+++ lib/screens/member_info.dart
@@ -0,0 +1,119 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:girl_scout_simple/components/constants.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
+import 'package:girl_scout_simple/components/member_container.dart';
+import 'package:girl_scout_simple/components/globals.dart';
+import 'package:girl_scout_simple/screens/members.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:simple_image_crop/simple_image_crop.dart';
+import 'package:girl_scout_simple/components/globals.dart' as globals;
+import 'package:girl_scout_simple/components/database_operations.dart';
+import 'package:girl_scout_simple/components/reusable_card.dart';
+
+class MemberInfo extends StatefulWidget {
+  //TODO: complete parameters
+  MemberInfo({@required this.data});
+  final Data data; //(ex) Add Member
+
+  static String id = '/MemberInfo';
+  @override
+  _AddState createState() => _AddState();
+}
+
+class _AddState extends State<MemberInfo> {
+
+  String name;
+  String team;
+  String gradeString;
+  String month;
+  int day;
+  int year;
+  String imageLocation;
+
+  @override
+  Widget build(BuildContext context) {
+    name = widget.data.name;
+    team = widget.data.team;
+    gradeString = widget.data.grade.toString();
+    month = widget.data.birthMonth;
+    day = widget.data.birthDay;
+    year = widget.data.birthYear;
+    imageLocation = widget.data.photoLocation;
+
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+
+        iconTheme: IconThemeData(
+          color: kWhiteColor, //change your color here
+        ),
+        title: Text(
+          "Member Information",
+          style: TextStyle(
+            color: kWhiteColor,
+            fontSize: 20.0,
+          ),
+        ),
+        backgroundColor: kDarkGreyColor,),
+      body: SingleChildScrollView(
+        child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+                children: <Widget>[
+                  Row(
+                      children: <Widget>[
+                        Expanded(
+                          flex: 4,
+                          child: Container(
+                            width: 150,
+                            height: 150,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                  image: FileImage(File(imageLocation)),
+                                  fit: BoxFit.scaleDown
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 10.0),
+                        Expanded(
+                          flex: 6,
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Text(name, style: Theme
+                                    .of(context)
+                                    .textTheme
+                                    .headline2,),
+                                Text(team, style: Theme
+                                    .of(context)
+                                    .textTheme
+                                    .subtitle1,),
+                              ]
+                          ),
+                        ),
+                      ]
+                  ),
+                  SizedBox(height: 10.0),
+                  ReusableCard(
+                    title: 'Patches', subtitle: 'All', addIcon: true,
+                    cardChild:
+                    ListView(
+                        shrinkWrap: true,
+                        children: null //TODO add an overloaded function to get scouts badges and progress
+                    ),
+                  ),
+
+                ]
+            )
+        ),
+      ),
+    );
+  }
+}
