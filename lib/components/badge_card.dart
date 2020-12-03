d49f79c48d9aa1cedfae21ad66f2a258bcb92509
Christiana <christianaramey@gmail.com>
2020-12-03 10:59:57 -0600

    removed patches for presentation

--- lib/components/badge_card.dart
+++ lib/components/badge_card.dart
@@ -28,7 +28,7 @@ class BadgeCard extends StatelessWidget {
       width: MediaQuery
           .of(context)
           .size
-          .width * 0.80,
+          .width * 0.800,
       decoration: BoxDecoration(
         color: kWhiteColor,
         border: Border.all(
